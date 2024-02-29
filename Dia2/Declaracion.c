039e9ece671631299cbf1117355157f0b8a82480
EmilioRivFa <160251465+EmilioRivFa@users.noreply.github.com>
2024-02-28 20:34:05 -0600

    Add files via upload

---
+++ Dia2/Declaracion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+int var1= 10;
+int var2= 20;
+int var3=2,
+int res= 0;
+res= var1*var2*var3;
+
+printf("El resultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res);
+
+
+printf("El resultado es %d", res);
+
+
+
+}
---
+++ Dia2/edad.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int mian (){
+    double r= 19 * 365;
+    double w= r * 24;
+    double g= w * 60;
+    double d= g * 60;
+
+    printf ("%d", d);
+
+    printf ("\n");
+    return 0;
+    
+
+}
+
+//double min= 60;
+    // hora= 60;
+    ///dia=24;
+    // año=;
+    ///edad = año*
